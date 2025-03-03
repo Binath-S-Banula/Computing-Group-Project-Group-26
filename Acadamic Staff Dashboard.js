9c793a28b2ae6f6305b39f99159b47489a2a50d3
NPVinodya <prabanivinodya@gmail.com>
2025-03-03 12:46:11 +0530

    Update js file💻 (Created UpdateTime Function for  current date and time📆⏰)

--- Acadamic Staff Dashboard.js
+++ Acadamic Staff Dashboard.js
@@ -13,3 +13,14 @@ if (loginBtn) {
         alert('Welcome to your profile');
     });
 }
+
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    const dateString = now.toLocaleDateString();
+    document.getElementById('digitalClock').textContent = timeString;
+    document.getElementById('currentTime').textContent = dateString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
